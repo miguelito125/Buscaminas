6736873bdee21da1107b24407e7affb2bff0207c
programacion <>
2020-03-20 11:19:31 +0100

    Con prueba unitaria
    cambios en Tablero y creando la prueba

--- src/model/Tablero.java
+++ src/model/Tablero.java
@@ -5,23 +5,24 @@ public class Tablero {
  * El modelo de datos aqui expresado es malo porque lo teneis que hacer bien vosotros
  */
 	private int [][] casillas;
-	private boolean [][] desveladas;
+	private boolean [][] veladas;
 	public Tablero() {
 		super();
 	}
 	public void setCasilas(int[][] casilas) {
 		this.casillas = casilas;
 	}
-	public void setDesveladas(boolean[][] desveladas) {
-		this.desveladas = desveladas;
-	}
 	public int[][] getCasilas() {
 		return casillas;
 	}
-	public boolean[][] getDesveladas() {
-		return desveladas;
-	}
 
+	
+	public boolean[][] getVeladas() {
+		return veladas;
+	}
+	public void setVeladas(boolean[][] veladas) {
+		this.veladas = veladas;
+	}
 	public boolean desvelarCasilla(int x,int y) {
 		assert inRango(0,casillas.length,x)&&inRango(0, casillas[0].length, y);
 		return false;
